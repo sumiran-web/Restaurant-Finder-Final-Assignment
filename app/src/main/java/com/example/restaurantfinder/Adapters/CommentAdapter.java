e0d441322455de0831ac5c0c7430e213aa02287e
sumiran <giri.sumiran111@gmail.com>
2020-01-02 19:28:32 +0545

    comment adapter error solved

--- app/src/main/java/com/example/restaurantfinder/Adapters/CommentAdapter.java
+++ app/src/main/java/com/example/restaurantfinder/Adapters/CommentAdapter.java
@@ -1,5 +1,11 @@
 package com.example.restaurantfinder.Adapters;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
 public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.Holder> {
 private Context context;
 private List<Reviews> reviews;
@@ -15,4 +21,28 @@ public CommentAdapter.Holder onCreateViewHolder(@NonNull ViewGroup viewGroup, in
         View v = LayoutInflater.from(context).inflate(R.layout.comment_list, viewGroup, false);
         return new Holder(v); {
 
+}
+    @Override
+    public void onBindViewHolder(@NonNull CommentAdapter.Holder holder, int i) {
+        final Reviews review = reviews.get(i);
+        holder.tv_cmts.setText(review.getReview());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class Holder extends RecyclerView.ViewHolder{
+        private Button btn_cmts;
+        private TextView tv_cmts;
+
+
+        public Holder(@NonNull View itemView) {
+            super(itemView);
+            btn_cmts.findViewById(R.id.btnComment);
+            tv_cmts.findViewById(R.id.tvComment);
+        }
+    }
 }
