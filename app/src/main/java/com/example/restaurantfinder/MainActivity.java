463ce767cc4cc47eda3fc3c793f85f82557c7af9
sumiran <giri.sumiran111@gmail.com>
2020-01-03 21:47:17 +0545

    Login and register in mainactivity

--- app/src/main/java/com/example/restaurantfinder/MainActivity.java
+++ app/src/main/java/com/example/restaurantfinder/MainActivity.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.restaurantfinder.Fragments.LoginFragment;
+import com.example.restaurantfinder.Fragments.Register;
 
+public class MainActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//
+        tabLayout=findViewById(R.id.Tablayout);
+        viewPager=findViewById(R.id.Viewpager);
+
+        ViewpageAdapter adapter = new ViewpageAdapter(getSupportFragmentManager());
+
+        adapter.addFragment(new Login(),"Login");
+     adapter.addFragment(new Register(),"Register");
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
+
