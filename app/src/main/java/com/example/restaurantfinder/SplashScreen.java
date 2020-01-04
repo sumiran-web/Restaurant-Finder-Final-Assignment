b7a79f0f8ab73b58b3114e4c8273dd522006cef9
sumiran <giri.sumiran111@gmail.com>
2020-01-04 20:24:13 +0545

    SplashScreen class code added

--- app/src/main/java/com/example/restaurantfinder/SplashScreen.java
+++ app/src/main/java/com/example/restaurantfinder/SplashScreen.java
@@ -1,4 +1,29 @@
 package com.example.restaurantfinder;
 
-public class SplashScreen {
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenpublic extends AppCompatActivity {
+    SharedPreferences sharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        final Intent intent = new Intent(SplashScreen.this, MainActivity.class );
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
+ {
 }
