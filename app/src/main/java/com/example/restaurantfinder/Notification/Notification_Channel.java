09023b6ea643b8c4e0328740baeeadec4aeee184
sumiran <giri.sumiran111@gmail.com>
2020-01-05 19:09:18 +0545

    Notification_Channel code added

--- app/src/main/java/com/example/restaurantfinder/Notification/Notification_Channel.java
+++ app/src/main/java/com/example/restaurantfinder/Notification/Notification_Channel.java
@@ -1,4 +1,22 @@
 package com.example.restaurantfinder.Notification;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+
 public class Notification_Channel {
+    Context context;
+    public final static String Channel_1 = "Channel1";
+
+    public Notification_Channel(Context context) {
+        this.context = context;
+    }
+
+    public void notificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    Channel_1, "Channel_1",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
 }
