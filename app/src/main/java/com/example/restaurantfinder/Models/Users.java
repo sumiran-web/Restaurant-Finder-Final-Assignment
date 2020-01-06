e7985029d669137cb2c9ef8675047b593dd08053
sumiran <giri.sumiran111@gmail.com>
2020-01-06 20:30:31 +0545

    Model_User code with some errors

---
+++ app/src/main/java/com/example/restaurantfinder/Models/Users.java
@@ -0,0 +1,24 @@
+package com.example.restaurantfinder.Models;
+
+public class Users {
+    private String Fname;
+    private String Lname;
+    private String Email;
+    private String Username;
+    private String Password;
+    private String Address;
+    private String Age;
+    private String Usertype;
+
+    public Users(String fname, String lname, String email, String username, String password, String address, String age, String usertype) {
+        Fname = fname;
+        Lname = lname;
+        Email = email;
+        Username = username;
+        Password = password;
+        Address = address;
+        Age = age;
+        Usertype = usertype;
+
+    }
+}
