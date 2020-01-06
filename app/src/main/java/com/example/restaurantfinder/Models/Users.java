7b3a981d59efb0daed86ac78c88e0dff9c380333
sumiran <giri.sumiran111@gmail.com>
2020-01-06 20:32:00 +0545

    Model_User Error Solved By Adding Getter and Setter Method

--- app/src/main/java/com/example/restaurantfinder/Models/Users.java
+++ app/src/main/java/com/example/restaurantfinder/Models/Users.java
@@ -21,4 +21,78 @@ public class Users {
         Usertype = usertype;
 
     }
+    public String getUsertype() {
+        return Usertype;
+    }
+
+    public void setUsertype(String usertype) {
+        Usertype = usertype;
+    }
+
+
+    public String getFname() {
+        return Fname;
+    }
+
+    public void setFname(String fname) {
+        Fname = fname;
+    }
+
+
+    public String getLname() {
+        return Lname;
+    }
+
+    public void setLname(String lname) {
+        Lname = lname;
+    }
+
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+
+    public String getAge() {
+        return Age;
+    }
+
+    public void setAge(String age) {
+        Age = age;
+    }
+
+
 }
+
+
