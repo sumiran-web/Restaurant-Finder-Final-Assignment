a5e585c17292a289cf45eef73414aefba852b1b3
sumiran <giri.sumiran111@gmail.com>
2020-01-06 20:29:01 +0545

    Model_Token_Generate code added

---
+++ app/src/main/java/com/example/restaurantfinder/Models/Token_Generate.java
@@ -0,0 +1,30 @@
+package com.example.restaurantfinder.Models;
+
+public class Token_Generate {
+    private String  token, user,_id;
+
+    public String get_id() {
+        return _id;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
+
