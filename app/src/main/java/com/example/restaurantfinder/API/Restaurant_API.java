ac698640c7c7b9049b6c90a3b849bfb84d742732
sumiran <giri.sumiran111@gmail.com>
2020-01-07 19:54:26 +0545

    Interface Restaurant _API Code Added

--- app/src/main/java/com/example/restaurantfinder/API/Restaurant_API.java
+++ app/src/main/java/com/example/restaurantfinder/API/Restaurant_API.java
@@ -1,5 +1,21 @@
 package com.example.restaurantfinder.API;
 
+import com.example.restaurantfinder.Models.Restaurants;
+import com.example.restaurantfinder.Models.Reviews;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Path;
+
 public interface Restaurant_API {
+
+    @GET("restaurants/showrestaurent")
+    Call<List<Restaurants>> getRestaurant(@Header("Authorization")String token);
+
+    @GET("restaurants/comment/{id}")
+    Call<List<Reviews>> getReview(@Path("id")String id);
 
 }
