70b5983f11985325ee88022fd3c1d00fb72806e2
sumiran <giri.sumiran111@gmail.com>
2020-01-08 19:45:09 +0545

    Login_BLL created

---
+++ app/src/main/java/com/example/restaurantfinder/BLL/LoginBLL.java
@@ -0,0 +1,5 @@
+package com.example.restaurantfinder.BLL;
+
+public class LoginBLL {
+
+}
--- app/src/main/java/com/example/restaurantfinder/Dashboard/Dashboard.java
+++ app/src/main/java/com/example/restaurantfinder/Dashboard/Dashboard.java
@@ -1,57 +1,70 @@
 package com.example.restaurantfinder.Dashboard;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.restaurantfinder.MainActivity;
+import com.example.restaurantfinder.R;
+import com.example.restaurantfinder.User_Activities.About_Us;
+import com.example.restaurantfinder.User_Activities.Restaurant_list;
+
+import static androidx.core.content.ContextCompat.startActivity;
+import static com.mapbox.mapboxsdk.Mapbox.getApplicationContext;
+
 public class Dashboard extends AppCompatActivity implements View.OnClickListener {
         TextView et_rest, et_update, et_aboutus, et_log_out;
 
 
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_dashboard);
 
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+                et_rest = findViewById(R.id.et_view_restaurant);
+                et_rest.setOnClickListener(this);
 
-        et_rest = findViewById(R.id.et_view_restaurant);
-        et_rest.setOnClickListener(this);
+                et_update = findViewById(R.id.et_update_profile);
+                et_update.setOnClickListener(this);
 
-        et_update= findViewById(R.id.et_update_profile);
-        et_update.setOnClickListener(this);
+                et_aboutus = findViewById(R.id.et_about_us);
+                et_aboutus.setOnClickListener(this);
 
-        et_aboutus= findViewById(R.id.et_about_us);
-        et_aboutus.setOnClickListener(this);
+                et_log_out = findViewById(R.id.et_logout);
+                et_log_out.setOnClickListener(this);
 
-        et_log_out=findViewById(R.id.et_logout);
-        et_log_out.setOnClickListener(this);
-
-        SharedPreferences preferences=getSharedPreferences("storetoken",0);
-        String token=preferences.getString("token",null);
-        String user=preferences.getString("username",null);
-        Toast.makeText(this, user, Toast.LENGTH_SHORT).show();
+                SharedPreferences preferences = getSharedPreferences("storetoken", 0);
+                String token = preferences.getString("token", null);
+                String user = preferences.getString("username", null);
+                Toast.makeText(this, user, Toast.LENGTH_SHORT).show();
         }
 
 
-@Override
-public void onClick(View v) {
-        switch (v.getId()){
-        case R.id.et_view_restaurant:
-        startActivity(new  Intent(this, Restaurant_list.class));
-        break;
+        @Override
+        public void onClick(View v) {
+                switch (v.getId()) {
+                        case R.id.et_view_restaurant:
+                                startActivity(new Intent(this, Restaurant_list.class));
+                                break;
 
-        case R.id.et_update_profile:
-        startActivity(new Intent(this, User_Update.class));
-        break;
+                        case R.id.et_update_profile:
+                                startActivity(new Intent(this, User_Update.class));
+                                break;
 
-        case R.id.et_about_us:
-        startActivity(new Intent(this, About_Us.class));
-        break;
+                        case R.id.et_about_us:
+                                startActivity(new Intent(this, About_Us.class));
+                                break;
 
-        case R.id.et_logout:
-        Intent intent = new Intent (Dashboard.this, MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
-        Toast.makeText(getApplicationContext(), "Log Out Successful", Toast.LENGTH_SHORT).show();
-        finish();
+                        case R.id.et_logout:
+                                Intent intent = new Intent(Dashboard.this, MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                                Toast.makeText(getApplicationContext(), "Log Out Successful", Toast.LENGTH_SHORT).show();
+                                finish();
+
+                }
 
         }
 
-        } {
 }
