2bf06578379d79f3f132b20d8b2bf5237ee0fe73
sumiran <giri.sumiran111@gmail.com>
2020-01-08 19:46:47 +0545

    Login_BLL code incomplete

--- app/src/main/java/com/example/restaurantfinder/BLL/LoginBLL.java
+++ app/src/main/java/com/example/restaurantfinder/BLL/LoginBLL.java
@@ -1,5 +1,33 @@
 package com.example.restaurantfinder.BLL;
 
+import java.io.IOException;
+
+import retrofit2.Response;
+
 public class LoginBLL {
+    private String username;
+    private String password;
+    private Token_Generate token_generate;
+    boolean isSuccess = false;
+
+    public LoginBLL(String username, String password) {
+        this.username = username;
+        this.password = password;
+
+    }
+
+    public boolean checkUser() {
+        try {
+            StrictMode();
+            Response<Token_Generate> response = API.createinstance().create(User_API.class).login(username, password).execute();
+            if (response.isSuccessful()) {
+                this.token_generate = response.body();
+                isSuccess = true;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return isSuccess;
+    }
 
 }
